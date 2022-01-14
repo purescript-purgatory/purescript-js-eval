cf4031d5d575143f84c44f75fdc7a1d3fddb1097
Rajat Sharma <lunasunkaiser@gmail.com>
2022-01-14 14:30:03 +0530

    Add more tests

--- test/Main.purs
+++ test/Main.purs
@@ -5,12 +5,28 @@ module Test.Main
 import Prelude
 
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Effect.Class.Console (log, logShow)
 import JSFFI (foreignEval)
 
 arch :: String
 arch = foreignEval "require('os').arch()"
 
+version :: String
+version = foreignEval "require('os').version()"
+
+concatenated :: String
+concatenated = foreignEval "'js'+'eval'"
+
+cpus :: Array { model :: String, speed :: Int }
+cpus = foreignEval "require('os').cpus()"
+
 main :: Effect Unit
 main = do
+  log concatenated
+  log version
   log arch
+  let speeds = (\x -> x.speed) `map` cpus
+  let models = (\x -> x.model) `map` cpus
+  logShow speeds
+  logShow models
+  pure unit
